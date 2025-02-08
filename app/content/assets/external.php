dc9fbef2c7ee928db32966c4e466bfd8820a57c1
Recep Bağıryanık <74258602+RecepBagiryanik@users.noreply.github.com>
2025-02-08 20:56:36 +0300

    Delete app directory

--- app/content/assets/external.php
+++
@@ -1,8 +0,0 @@
-<?php
-define("__ROOT__", __DIR__ . "/../../../");
-require_once(__DIR__ . "/../../library/logger.php");
-require_once(__DIR__ . "/../../library/public/external.php");
-
-use Public\External\Control;
-
-control::get($_GET["fileLocation"], __DIR__);
--- app/middlewares/auth.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace HTTP\User;
-
-class Auth {
-    public static function reg() {
-        return true;
-    }
-}
